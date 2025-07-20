987325a3d298de4d5e1319e3ca6de2b72c495ce7
Precise Analytics <kipnorm@preciseanalytics.io>
2025-07-20 16:58:30 -0400

    Fix LinkedIn icons on team page - replace missing SVG with inline component

--- pages/about-us/team.tsx
+++ pages/about-us/team.tsx
@@ -9,6 +9,13 @@ import Container from 'components/Container';
 import { EnvVars } from 'env';
 import { media } from 'utils/media';
 
+// LinkedIn Icon Component
+const LinkedInIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+  </svg>
+);
+
 const teamMembers = [
   {
     name: 'Norman Tanui',
@@ -81,17 +88,7 @@ export default function TeamPage() {
                 <Bio>{member.bio}</Bio>
                 {member.linkedin && (
                   <LinkedInLink href={member.linkedin} target="_blank" rel="noopener noreferrer">
-                    <Image 
-                      src="/linkedin-icon.svg" 
-                      alt="LinkedIn" 
-                      width={24}
-                      height={24}
-                      style={{
-                        display: 'block',
-                        width: '24px',
-                        height: '24px'
-                      }}
-                    />
+                    <LinkedInIcon />
                   </LinkedInLink>
                 )}
               </TeamCard>
@@ -99,8 +96,6 @@ export default function TeamPage() {
           </TeamGrid>
         </Container>
       </PageWrapper>
-
-      
     </>
   );
 }
@@ -177,5 +172,18 @@ const Bio = styled.p`
 
 const LinkedInLink = styled.a`
   display: inline-block;
-  margin-top: 1rem;
+  margin-top: 1.5rem;
+  color: #0077b5;
+  transition: all 0.3s ease;
+  
+  &:hover {
+    color: #005885;
+    transform: scale(1.1);
+  }
+  
+  svg {
+    display: block;
+    width: 24px;
+    height: 24px;
+  }
 `;

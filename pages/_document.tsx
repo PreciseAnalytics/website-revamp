17d1212199b609cff229d187bd5360a1d070a346
Precise Analytics <kipnorm@preciseanalytics.io>
2026-04-18 10:36:07 -0400

    Remove external Google font links from document head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,16 +34,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Fonts */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-
           {/* Theme */}
           <meta name="theme-color" content="#0F172A" />
         </Head>
